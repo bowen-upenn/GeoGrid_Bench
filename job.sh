14c47331ee7c927e9f5c8a788ba3a33ab4e6a866
Yangxinyu <xinyux@wharton.upenn.edu>
2025-04-19 20:16:46 +0000

    job

--- job.sh
+++ job.sh
@@ -30,33 +30,6 @@ exec "$@"
 EOF
 chmod +x set_affinity_gpu_polaris.sh
 
-# Create bash script for running commands
-cat > run_inference_task.sh << 'EOF'
-#!/bin/bash
-
-# Get rank from MPI environment
-RANK=$PMI_RANK
-SIZE=$PMI_SIZE
-
-# Define all tasks
-declare -a TASKS=(
-  "bash ./scripts/run_inference_llama_3p2_11b_vision.sh text 0 -1"
-  "bash ./scripts/run_inference_llama_3p2_11b_vision.sh code 0 -1"
-  "bash ./scripts/run_inference_llama_3p2_3b.sh text 0 -1"
-  "bash ./scripts/run_inference_llama_3p2_3b.sh code 0 -1"
-  "bash ./scripts/run_inference_llama_3p1_8b.sh text 0 -1"
-  "bash ./scripts/run_inference_llama_3p1_8b.sh code 0 -1"
-)
-
-# Calculate which tasks this rank should handle
-for i in "${!TASKS[@]}"; do
-  if [ $((i % SIZE)) -eq $RANK ]; then
-    echo "RANK $RANK executing task $i: ${TASKS[$i]}"
-    eval "${TASKS[$i]}"
-    echo "RANK $RANK completed task $i"
-  fi
-done
-EOF
 chmod +x run_inference_task.sh
 
 # Launch MPI job
